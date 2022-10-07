61c27ec127d5b10d67d0b516db35837602e7b23e
Ahmed Saadawy <assassinscool625@gmail.com>
2022-10-07 14:21:43 +0200

    testing heroku

--- final_project/__init__.py
+++ final_project/__init__.py
@@ -4,7 +4,7 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
 app = Flask(__name__)
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+##app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://rmbzggmgwckisa:d3cb08acf1ff822d2b30efde57b540309d811c738bede3847cb8defcff9ac15c@ec2-3-214-2-141.compute-1.amazonaws.com:5432/d76kf0sbgv38ql'
 
